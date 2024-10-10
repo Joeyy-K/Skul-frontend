24accb55b31b67e065ff955d05fa63f1836bc05b
joeyy-k <joeykkdr@gmail.com>
2024-10-10 03:15:47 +0300

    trying to resolve issues

--- skul/vite.config.js
+++ skul/vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      external: ['@rollup/rollup-linux-x64-gnu']
+    }
+  },
   plugins: [react()],
   resolve: {
     alias: {
